d8a5d6a91c4826e20ff3d5d9dead8174d9c54015
Michael Ewnetu <143202037+Ewnetu-Michael-27@users.noreply.github.com>
2023-10-16 11:51:47 -0400

    Update Heart_built.py

--- Heart_built.py
+++ Heart_built.py
@@ -97,7 +97,7 @@ fig = px.parallel_coordinates(df_heart[["age","thalach","oldpeak","num"]], color
                 "age": "Age", "thalach": "Thalach",
                 "oldpeak": "Oldpeak",},
                              #color_continuous_scale=px.colors.diverging.Tealrose,
-                             color_continuous_midpoint=2)
+                             #color_continuous_midpoint=2)
 st.plotly_chart(fig)
 
 
